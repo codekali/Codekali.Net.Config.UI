e23d68ecb188e64c1f8b934773e357646abb8877
Jawwad Ayyubi <jawwadayyubi15@gmail.com>
2026-09-10 15:49:44 +0530

    Configuration tests

---
+++ samples/SampleWebApp/CustomConfigurationTests.cs
@@ -0,0 +1,15 @@
+﻿using Codekali.Net.Config.UI.Interfaces;
+
+namespace SampleWebApp;
+public class CustomConfigurationTests : IConfigurationTest
+{
+    public string Name => "Production Readiness";
+
+    public Task<AssertionOutcome> RunAsync(IConfiguration config, CancellationToken ct)
+    {
+        if (string.IsNullOrWhiteSpace(config["ConnectionStrings:Default"]))
+        return Task.FromResult(AssertionOutcome.Fail("ConnectionStrings:Default must not be empty in Production."));
+    
+        return Task.FromResult(AssertionOutcome.Pass());
+    }
+}
